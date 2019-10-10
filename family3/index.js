9b8a9af7311d2bda7dfd4f1e21262a26f7e187b9
Jack p <jtv199@gmail.com>
2019-10-10 17:23:17 +1100

    foundation for relationship page, also immediatly swaps to it

--- family3/index.js
+++ family3/index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import {RelationshipPage} from './src/page/RelationshipPage/RelationshipPage'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => RelationshipPage);
---
+++ family3/src/page/RelationshipPage/RelationshipPage.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react'
+import{ View, Text, StyleSheet, Button, StatusBar} from 'react-native';
+import { Header, Left, Right, Button as ButtonBase , Body, Title } from 'native-base'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+import { Color } from '../assets/Assets'
+
+export default class RelationshipPage extends Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        return (
+            <View style={styles.MainContainer}>
+                <Header style = {styles.headerContainer}>
+                    <StatusBar
+                        backgroundColor={Color.STATUS_BAR}
+                        barStyle="dark-content"
+                    />
+                    <Left>
+                        <ButtonBase
+                            transparent
+                            onPress={() => this.props.navigation.openDrawer()}
+                            >
+                            <Icon name="navicon" size={20} color= {Color.SECONDARY}/>
+                        </ButtonBase>
+                    </Left>
+                    <Body>
+                        <Title style = {{color:Color.SECONDARY}}>Sample</Title>
+                    </Body>
+                    <Right />
+                </Header>
+                <View style = {styles.contentContainer}>
+                    <Text style = {{color: Color.SECONDARY}}>
+                        Sample Page
+                    </Text>
+                </View>
+
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    MainContainer: {
+		flex: 1,
+        backgroundColor: Color.PRIMARY
+    },
+
+    contentContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    headerContainer: {
+        alignItems: 'flex-start',
+        backgroundColor: Color.PRIMARY
+    },
+})
