88ea640605069869af17ace8d5616b9d433d628d
ChouTatsumi <zhangxunsnail@gmail.com>
2019-10-20 21:18:02 +1100

    ready for loading test

--- family3/src/page/AddRelationshipsPage/AddRelationship.js
+++ family3/src/page/AddRelationshipsPage/AddRelationship.js
@@ -98,11 +98,7 @@ export default class AddRelationship extends Component {
 
     // retrieve stored info when app loads
     componentDidMount = () => {
-        const googleUserData = await GoogleSignin.getCurrentUser()
-
-        const userid = googleUserData.user.id;
-
-        const userData = (await APIHandler.getAccount(userid)).data;
+        const userData = await this.DBHandler.getDBUserData()
 
         const familyid = userData.family;
 
